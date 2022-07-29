d61a24f8fed5d0a6aafc61eb36f9f531e856c738
Lucas L. Pacheco <luccaslp99@gmail.com>
2022-07-29 19:11:41 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine} from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -9,12 +11,22 @@ export function Sidebar(){
             />
         
             <div className={styles.profile}>
+                <img 
+                    className={styles.avatar} 
+                    src="https://avatars.githubusercontent.com/u/73972488?v=4" 
+                    alt=""
+                />
+
                 <strong>Lucas Pacheco</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href='#'>Editar seu perfil</a>
+                
+                <a href='#'>
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     );
