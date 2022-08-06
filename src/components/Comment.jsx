f8e754adbd338f935e94ab4286d03b3c3d0c8a9b
Lucas L. Pacheco <luccaslp99@gmail.com>
2022-08-06 13:37:14 -0300

    Estrutura da aplicação - Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return(
+        <div className={styles.comment}>
+            <img src="https://github.com/dev-lucccaslp.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={style.authorAndTime}>
+                            <strong>Lucas Pacheco</strong>
+                            <time title='29 de Julho às 20:40h' dateTime='2022-07-29'>Cerca de 1h atrás.</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
