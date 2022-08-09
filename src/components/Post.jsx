ff4f429a756c9d33065efa2c98c13cd599272d6e
Lucas L. Pacheco <luccaslp99@gmail.com>
2022-08-08 21:39:24 -0300

    Propriedades do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,32 +1,52 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptbr from 'date-fns/locale/pt-BR'
+
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 import styles from './Post.module.css'
 
-export function Post(props){
+export function Post({ author, publishedAt, content }){
+    // const publishedDateFormated = new Intl.DateTimeFormat('pt-BR', {
+    //   day: "2-digit", 
+    //   month: "long",
+    //   hour: "2-digit",
+    //   minute: "2-digit",
+
+    // }).format(publishedAt)
+
+    const publishedDateFormated = format(publishedAt,"dd 'de' LLLL 'às' HH:mm'h' ", {
+        locale: ptbr
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptbr,
+        addSuffix: true,
+    })
+
     return(
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar hasBorder={true} src='https://avatars.githubusercontent.com/u/73972488?v=4' />
+                    <Avatar hasBorder={true} src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Lucas Pacheco</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title='29 de Julho às 20:40h' dateTime='2022-07-29'>Publicado há 1h</time>
+                <time title={publishedDateFormated} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
 
             </header>
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-                <p><a href=''>jane.design/doctorcare</a></p>
-
-                <p>
-                    <a href=''>#novoprojeto</a>{' '}
-                    <a href=''>#nlw</a>{' '}                        <a href=''>#rocketseat</a>
-                </p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type === 'link'){
+                        return <p><a href=''>{line.content}</a></p>
+                    }
+                })}
 
             </div>
 
@@ -36,7 +56,7 @@ export function Post(props){
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
-                    placeholder='Deixe um coment[ario'
+                    placeholder='Deixe um comentario'
                 />
                 <footer>
                     <button type='submit'>Publicar</button>
